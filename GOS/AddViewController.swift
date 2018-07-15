77f85efe95eacfef9862c2c2ff1fad5af58152dd
한병두 <ByungdooHan@hanbyeongduui-MacBook-Pro.local>
2018-07-15 21:05:14 +0900

    move view when keyboard appears

--- GOS/AddViewController.swift
+++ GOS/AddViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
     @IBOutlet weak var listOfSports: UIPickerView!
     @IBOutlet weak var addVIew_Time: UITextField!
@@ -17,19 +17,38 @@ class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     @IBOutlet weak var addView_Detail: UITextView!
     
     var sports = ["농구","축구","배구","테니스","야구","배드민턴","탁구","아이스 하키"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+                
         addView_Detail.layer.borderWidth = 0.5
         addView_Detail.layer.borderColor = UIColor.lightGray.cgColor
         addView_Detail.layer.cornerRadius = 5.0
+        self.hideKeyboardTappedAround()
         
-    }
+        NotificationCenter.default.addObserver(self, selector: #selector(AddViewController.keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(AddViewController.keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
   
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
+    
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -55,4 +74,24 @@ class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     }
     */
 
+}
+
+extension AddViewController {
+    func hideKeyboardTappedAround() {
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
 }
